88586cdc7c85432fb2e63a2710872e206f11afb3
zhiweicoding <diaozhiwei2k@163.com>
2023-02-07 15:47:58 +0800

    init

---
+++ thought-common/src/main/java/com/zhiweicoding/common/constant/KeyConstant.java
@@ -0,0 +1,11 @@
+package com.zhiweicoding.common.constant;
+
+/**
+ * <h1>通用的jwt的key</h1>
+ *
+ * @author by diaozhiwei on 2023/02/07.
+ * @email diaozhiwei2k@163.com
+ */
+public class KeyConstant {
+    public static final String PUBLIC_KEY = "";
+}
---
+++ thought-common/src/main/java/com/zhiweicoding/common/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author by diaozhiwei on 2023/02/07.
+ * @email diaozhiwei2k@163.com
+ */
+package com.zhiweicoding.common;
