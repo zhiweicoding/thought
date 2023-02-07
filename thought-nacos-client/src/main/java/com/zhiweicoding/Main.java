a0144b4effb42cb0035bb3bc631a47e1dbd3978e
zhiweicoding <diaozhiwei2k@163.com>
2023-02-07 15:23:12 +0800

    init

---
+++ thought-nacos-client/src/main/java/com/zhiweicoding/Main.java
@@ -0,0 +1,11 @@
+package com.zhiweicoding;
+
+/**
+ * @author by diaozhiwei on ${YEAR}/${MONTH}/${DAY}.
+ * @email diaozhiwei2k@163.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
