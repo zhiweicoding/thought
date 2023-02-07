95cd0f72c722837a43f256090e26867bbfda638f
zhiweicoding <diaozhiwei2k@163.com>
2023-02-08 01:16:52 +0800

    nacos base client

--- thought-nacos-client/src/main/java/com/zhiweicoding/nacos/NacosClientApplication.java
+++ thought-nacos-client/src/main/java/com/zhiweicoding/nacos/NacosClientApplication.java
@@ -1,19 +1,19 @@
 package com.zhiweicoding.nacos;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author by diaozhiwei on 2023/02/07.
  * @email diaozhiwei2k@163.com
  */
-@ComponentScan
-@SpringBootConfiguration
+@ServletComponentScan
 @EnableDiscoveryClient
 @RefreshScope
+@SpringBootApplication
 public class NacosClientApplication {
 
     public static void main(String[] args) {
--- thought-nacos-client/src/main/java/com/zhiweicoding/nacos/config/ProjectConfig.java
+++ thought-nacos-client/src/main/java/com/zhiweicoding/nacos/config/ProjectConfig.java
@@ -1,10 +1,7 @@
 package com.zhiweicoding.nacos.config;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 /**
