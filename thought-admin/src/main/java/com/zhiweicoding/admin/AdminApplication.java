fd1c4147a23fab718cec6dfc3cf9c52e0d9d56d6
zhiweicoding <diaozhiwei2k@163.com>
2023-02-11 21:46:13 +0800

    admin pom

---
+++ thought-admin/src/main/java/com/zhiweicoding/admin/AdminApplication.java
@@ -0,0 +1,18 @@
+package com.zhiweicoding.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by diaozhiwei on 2023/02/10.
+ * @email diaozhiwei2k@163.com
+ */
+@EnableDiscoveryClient
+@RefreshScope
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
