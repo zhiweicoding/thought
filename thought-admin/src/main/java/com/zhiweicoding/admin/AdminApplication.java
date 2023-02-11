a623959afb71e28d5e6286b92a63326cc12ff8c6
zhiweicoding <diaozhiwei2k@163.com>
2023-02-11 21:51:06 +0800

    admin pom update

--- thought-admin/src/main/java/com/zhiweicoding/admin/AdminApplication.java
+++ thought-admin/src/main/java/com/zhiweicoding/admin/AdminApplication.java
@@ -2,6 +2,8 @@ package com.zhiweicoding.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * @author by diaozhiwei on 2023/02/10.
